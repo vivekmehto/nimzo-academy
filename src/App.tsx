3fe8b99cd7c936e225b502e6e7c69c59e7328dc5
vivekmehto <vivekmehto.webdev@gmail.com>
2026-01-16 11:29:37 +0530

    Feat: Whatsapp foater button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { BrowserRouter, Routes, Route} from "react-router";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import RefundPolicy from "./pages/RefundPolicy";
+
+import ScrollToTop from "./utils/ScrollToTop";
+import WhatsAppButton from "./components/WhatsAppButton";
+
 function App() {
   return (
-    <>
+  <BrowserRouter>
+  <ScrollToTop />
     <Header />
-      <Home />
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+      <Route path="/refund-policy" element={<RefundPolicy />} />
+
+    </Routes>
+   <WhatsAppButton />
     <Footer />
-    </>
+  </BrowserRouter>
+    
   );
 }
 
