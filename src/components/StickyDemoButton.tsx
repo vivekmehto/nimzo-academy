4abbace1cf95d778bda593c3f4acf00fd82c699b
vivekmehto <vivekmehto.webdev@gmail.com>
2026-01-28 19:22:38 +0530

    update sticky button on mobile screen

--- src/components/StickyDemoButton.tsx
+++ src/components/StickyDemoButton.tsx
@@ -8,15 +8,14 @@ const StickyDemoButton: React.FC<Props> = ({ onClick }) => {
   return (
     <button
       onClick={onClick}
-      
-      className="
-  fixed right-0 top-1/2 z-40 -translate-y-1/2
-  -rotate-90 origin-bottom-right
-  rounded-t-lg bg-amber-500 px-4 py-2
-  text-sm font-semibold text-white
-"
-
       aria-label="Book Free Demo"
+      className="
+        md:hidden fixed bottom-4 left-1/2 z-40
+    -translate-x-1/2
+    rounded-full bg-amber-500
+    px-6 py-3 text-sm font-semibold text-white
+    shadow-lg
+  "
     >
       Book Free Demo
     </button>
