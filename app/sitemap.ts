5322f6434f6f2c1dbe9158a25fc232d1099adca0
vivekmehto <vivekmehto.webdev@gmail.com>
2026-02-19 14:37:26 +0530

    ADD: sitemap.ts file

---
+++ app/sitemap.ts
@@ -0,0 +1,50 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://nimzoacademy.com/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/programs",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/gallery",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/contact",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/recommended-chess-gear",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/privacy-policy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/terms-and-conditions",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/refund-policy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/disclaimer",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://nimzoacademy.com/child-safety-policy",
+      lastModified: new Date(),
+    },
+  ];
+}
